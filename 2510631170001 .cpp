56d2da83ebb95a41d6ebd581f8406d6638d2d515
privasikaisya-sketch <privasikaisya@gmail.com>
2025-09-06 20:15:02 +0700

    Create 2510631170001 .cpp

---
+++ 2510631170001 .cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string judul;
+    cout << "Di sebuah desa kecil yang dikelilingi padang rumput hijau, hiduplah seorang anak laki-laki bernama Miko. Miko adalah anak yang baik hati dan selalu ditemani oleh anjing kecilnya yang setia, Bobby. Mereka berdua sangat akrab dan selalu bermain bersama di padang rumput setiap hari\n";
+    cout << "Suatu pagi, saat Miko dan Bobby sedang berlari-lari di padang rumput, Miko melihat sesuatu yang bersinar di antara rerumputan. la mendekati benda itu dan menemukan sebuah daun semanggi dengan empat helai. Daun itu berkilauan seolah-olah menyimpan keajaiban.\n";
+    cout << "Dengan penuh rasa ingin tahu, Miko memetik daun semanggi itu dan menunjukkan kepada Bobby. Lihat, Bobby Aku menemukan daun semanggi empat yang ajaib Menurut cerita nenek, ini bisa membawa keberuntungan\n";
+    cout << "Bobby menggonggong riang, seolah-olah mengerti bahwa sesuatu yang luar biasa akan terjadi. Miko kemudian memikirkan permohonan yang ingin ia buat. la menatap daun semanggi itu dan berkata dengan hati yang tulus, Aku ingin semua orang di desaku selalu bahagia dan tidak pernah merasa kesepian.\n";
+    cout << "Tiba-tiba, daun semanggi itu bersinar terang dan berubah menjadi debu bercahaya yang menyebar ke seluruh desa. Sejak saat itu, hal-hal ajaib mulai terjadi. Tanaman di ladang tumbuh subur, sungai yang mengalir menjadi lebih jernih, dan suasana di desa menjadi lebih hangat dan penuh kebahagiaan.\n";
+    cout << "Namun, keajaiban terbesar adalah munculnya banyak anjing kecil yang lucu di desa. Setiap keluarga yang merasa kesepian sekarang memiliki teman baru yang setia, seperti Bobby bagi Miko. Desa yang dulu sunyi kini penuh dengan tawa dan kegembiraan.\n";
+    cout << "Miko sangat bahagia melihat desa mereka menjadi tempat yang lebih ceria. la menyadari bahwa daun semanggi ajaib itu telah membantu membawa kebahagiaan bagi semua orang, dan ia sangat bersyukur\n";
+    cout << "Setiap hari, Miko dan Bobby terus bermain di padang rumput, tetapi sekarang mereka ditemani oleh teman-teman baru. Miko belajar bahwa kebahagiaan sejati datang dari berbagi dan keinginan untuk membuat orang lain bahagia\n";
+    
+    return 0;
+}
