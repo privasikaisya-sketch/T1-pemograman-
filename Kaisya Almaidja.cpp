48ba34824c50a2d3a9058b6e29b426ba8e7a15d8
privasikaisya-sketch <privasikaisya@gmail.com>
2025-09-06 20:10:24 +0700

    Create Kaisya Almaidja.cpp

---
+++ Kaisya Almaidja.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string nama;
+    int usia;
+
+    cout << "Masukkan Nama: ";
+    cin >> nama;
+
+    cout << "Masukkan Usia: ";
+    cin >> usia;
+
+    cout << "Hallo " << nama << ", usia Anda " << usia << " tahun." << endl;
+
+    return 0;
+}
